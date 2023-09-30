cc0f3ac45e68eee5c9a0d8310cf02f893e4d4285
Andrey Xoloshef <Xoloshef@gmail.com>
2023-09-30 12:33:18 +0300

    start

---
+++ src/main/java/com/example/deliveryexpress/DeliveryExpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryExpressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deliveryexpress/DeliveryExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliveryexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryExpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
